a365aff72509680cf13700381c2468de36e35895
Joerg Walter <jwdev@posteo.de>
2021-07-13 14:58:18 +0200

    -more flexible http handling. -added support for sending an Accept-Language field in http requests. -implemented support for command line option "lang".

--- imdb2mkvtags.go
+++ imdb2mkvtags.go
@@ -4,20 +4,26 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
-	"github.com/jwdev42/imdb2mkvtags/internal/http"
+	ihttp "github.com/jwdev42/imdb2mkvtags/internal/http"
 	"github.com/jwdev42/imdb2mkvtags/internal/imdb"
 	"github.com/jwdev42/imdb2mkvtags/internal/tags"
 	"os"
+	"strings"
 )
+
+func die(err error) {
+	fmt.Fprintf(os.Stderr, "%s\n", err)
+	os.Exit(1)
+}
 
 func write(file *os.File, data *tags.Movie) {
 	if file != os.Stdout {
 		defer file.Close()
 	}
 	if err := tags.WriteTags(file, data.WriteTag); err != nil {
-		fmt.Fprintf(os.Stderr, "Error writing output: %s\n", err)
-		os.Exit(1)
+		die(fmt.Errorf("Error writing output: %s", err))
 	}
 }
 
@@ -26,31 +32,35 @@ func main() {
 	flags := cmdFlags()
 
 	if len(flags.tail) < 1 {
-		fmt.Fprintf(os.Stderr, "No IMDB title id found\n")
-		os.Exit(0)
+		die(errors.New("No IMDB title id found"))
 	}
 	url, err := imdb.Id2Url(flags.tail[0])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Invalid Input: %s\n", err)
-		os.Exit(1)
+		die(fmt.Errorf("Invalid Input: %s", err))
+	}
+	req, err := ihttp.NewBareReq("GET", url, nil)
+	if err != nil {
+		die(fmt.Errorf("Could not create HTTP request: %s", err))
+	}
+	if len(*flags.lang) > 0 {
+		if err := ihttp.SetReqAccLang(req, strings.Split(*flags.lang, ",")...); err != nil {
+			die(fmt.Errorf("Could not set header for \"Accept-Language\": %s", err))
+		}
 	}
 	buf := new(bytes.Buffer)
-	if err := http.Body(url, buf); err != nil {
-		fmt.Fprintf(os.Stderr, "Error receiving webpage: %s\n", err)
-		os.Exit(1)
+	if err := ihttp.Body(nil, req, buf); err != nil {
+		die(fmt.Errorf("Error receiving webpage: %s", err))
 	}
 	sMovie, err := imdb.ExtractMovieSchema(buf)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while extracting movie schema: %s\n", err)
-		os.Exit(1)
+		die(fmt.Errorf("Error while extracting movie schema: %s", err))
 	}
 	var file *os.File
 	if *flags.out != "" {
 		var err error
 		file, err = os.Create(*flags.out)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
-			os.Exit(1)
+			die(err)
 		}
 	} else {
 		file = os.Stdout
--- internal/http/http.go
+++ internal/http/http.go
@@ -3,31 +3,61 @@
 package http
 
 import (
+	"errors"
 	"io"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
-func Body(url string, dest io.Writer) error {
-	resp, err := http.Get(url)
+const UserAgent = "imdb2mkvtags/1.0"
+
+var regexpLang = regexp.MustCompile("^[a-z]{2}(-[A-Z]{2})?$")
+
+func Body(client *http.Client, req *http.Request, dest io.Writer) error {
+	if client == nil {
+		client = new(http.Client)
+	}
+	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-	buf := make([]byte, 4096)
-	for {
-		n, err := resp.Body.Read(buf)
-		if n > 0 {
-			_, err := dest.Write(buf[:n])
-			if err != nil {
-				return err
-			}
-		}
-		if err == io.EOF {
-			break
-		} else if err != nil {
+	if _, err := io.Copy(dest, resp.Body); err != nil {
+		return err
+	}
+	return nil
+}
+
+//Returns a new http request with default header fields
+func NewBareReq(method, url string, body io.Reader) (*http.Request, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", UserAgent)
+	req.Header.Set("Accept-Charset", "utf-8")
+	return req, nil
+}
+
+//Sets the passed language strings as the request's "Accept-Language" header field.
+//Rejects formally malformed language strings to prevent header field injection.
+func SetReqAccLang(req *http.Request, lang ...string) error {
+	if req.Header == nil {
+		req.Header = make(http.Header)
+	}
+	for _, v := range lang {
+		if err := chkLang(v); err != nil {
 			return err
 		}
+	}
+	req.Header.Set("Accept-Language", strings.Join(lang, ","))
+	return nil
+}
 
+func chkLang(s string) error {
+	if !regexpLang.MatchString(s) {
+		return errors.New("Malformed language string")
 	}
 	return nil
 }
