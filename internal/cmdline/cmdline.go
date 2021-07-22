52cd7b16c5727e3079fabd5c3aaae920148f5724
Joerg Walter <jwdev@posteo.de>
2021-07-22 14:18:49 +0200

    -added support in cmdline for imdb options.

--- internal/cmdline/cmdline.go
+++ internal/cmdline/cmdline.go
@@ -6,16 +6,19 @@ import (
 	"flag"
 )
 
+//Structure that holds the parsed command line flags
 type Flags struct {
 	Out  *string  //output file
-	Lang *string  //language passed to the imdb website
-	Tail []string //non-processes args
+	Lang *string  //language passed to the website
+	Imdb *string  //options for the imdb scraper
+	Tail []string //non-processed args
 }
 
 func Parse() *Flags {
 	f := new(Flags)
 	f.Out = flag.String("o", "", "sets the output file")
 	f.Lang = flag.String("lang", "", "sets the preferred language for http requests")
+	f.Imdb = flag.String("imdb", "", "options for the imdb scraper, separated by a colon")
 	flag.Parse()
 	f.Tail = flag.Args()
 	return f
